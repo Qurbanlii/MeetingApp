988fa944af71ca164466ead0f74fa0fcb8f945c6
Ali Qurbanli <aliqurbanli2007@gmail.com>
2025-07-10 16:00:59 +0400

    elave edildi

--- obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b291c416908873b2280edfcb710fa06396f6bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
